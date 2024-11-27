c44abd843d624861ab77b8e938ef014c460a70fd
Dzaky-Hadyan <dzakyhadyaneliyanta06@gmail.com>
2024-11-27 14:58:42 +0700

    feat: adding tugas 3

---
+++ CekPrimaRekursif.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CekPrimaRekursif {
+    static String cekPrima(int n, int i) {
+        if (i >= 2) {
+            if (n % i != 0) {
+                return cekPrima(n, i - 1);
+            } else {
+                return "Bukan Prima";
+            }
+        } else {
+            return "Prima";
+        }
+        // 5 % 4 != 0 (5 % 4 != 0 && i >= 2) lanjut karena != dan >= terpenuhi
+        // 5 % 3 != 0 (5 % 3 != 0 && 3 >= 2) lanjut karena != dan >= terpenuhi
+        // 5 % 2 != 0 (5 % 2 != 0 && 2 >= 2) lanjut karena != dan >= terpenuhi
+        // (5 % 1 != 0 && 1 >= 2) tidak lanjut karena keduanya tidak terpenuhi
+        // "Prima"
+        // 4 % 3 != 0 (4 % 3 != 0 && 3 >= 2) lanjut karena != dan >= terpenuhi
+        // 4 % 2 = 0 (4 % 2 != 0 && 2 >= 2) tidak lanjut karena pertama tidak terpenuhi
+        // "Bukan Prima"
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan bilangan: ");
+        int bilangan = sc.nextInt();
+        int i = bilangan - 1;
+        System.out.println(cekPrima(bilangan, i));
+        sc.close();
+    }
+}
+// cekPrima(5) = 5 % 4 != 0
+//               5 % 3 != 0
+//               5 % 2 != 0
+// cekPrima(6) = 6 % 5 != 0
+//               6 % 4 != 0
+//               6 % 3 = 0
+// base case = n = 2
+// recursion call = f(n) = n % n-1 != 0
